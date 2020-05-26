e54500ad213ed08c881538ec1b7f2dea0bb37a38
Aygün BALTACI <aygunbaltaci@gmail.com>
2020-05-26 13:05:14 +0200

    Codes are merged, uplink does not work.

--- downlink.py
+++ downlink.py
@@ -3,10 +3,10 @@
 #####################################################
 # 25.05.2020
 #
-# This code generates UAV data traffic for
-# downlink channel (from UAV to remote controller).
+# This code generates UAV data traffic according to
+# distribution models based on actual UAV data. 
 # 
-# Prerequisites: pip3 install msvcrt numpy scapy
+# Prerequisites: pip3 install keyboard numpy scapy
 #
 # Author: Aygün Baltaci
 # Institution: Technical University of Munich
@@ -24,43 +24,75 @@ import keyboard
 import time
 import numpy as np
 
-# ======== variables
+# ======== variables - modify them as you wish =========
 outputfolder = "outputfiles"
 ip_source = "10.0.0.201" 
 ip_destination = "10.0.0.208" 
 port_source = 47813 
+port_destination = 47814
+pkt_length_maximum = 1486
 date = datetime.now().strftime('%Y%m%d_%H%M%S')
-buffercheck_frequency = 3 # in how many CPU cycles the buffer to be checked
+# ========================================
+# Frequencies of data generation. 
+# Each number corresponds to in how many CPU 
+# cycles the parameter is generated.
+# ========================================
+# downlink
+frequency_buffer = 3 
+frequency_camera_control_record_video = 7 
+frequency_land_takeoff = 3
+frequency_pitch_roll = 2
+frequency_return_home_trim = 5
+frequency_throttle_yaw = 1
+# uplink
+frequency_batterystatus_camerastatus = 6
+frequency_buffer = 3
+frequency_imustatus_rotorstatus = 2
+frequency_video = 1
 
 def pkt_create(payload):
 	pkt = IP() / UDP() / Raw(load = payload)
-	#pkt = pkt
-	pkt.time = time.time() * 100
 	pkt[IP].src = ip_source
 	pkt[IP].dst = ip_destination
-	pkt[UDP].sport = port_source # Add a src port #
+	pkt[UDP].sport = port_source 
+	pkt[UDP].dport = port_destination
 	return pkt
+
+def generate_stats(datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous):
+	time_difference = float(pkt.time - time_previous) * 1000 if time_previous != 0 else 0 # multiply by 1000 to convert into ms
+	pkt_interarrival.append(time_difference)
+	pkt_length.append(len(pkt))
+	if int(time_previous) != int(pkt.time): # divide by 100 to convert into sec
+		if not firstrun: datarate.append(pkt_length_total * 8 / 1000) # multiply by 8 to convert bytes to bits, divide by 1000 to convert into kbps
+		pkt_length_total = 0
+		firstrun = False
+	else:
+		datarate.append("")
+	pkt_length_total += len(pkt)	
+	pkt_list.append(pkt) # list_pkt are the generated packets to be sent to the MAC layer for transmission
+	time_previous = pkt.time
+	return datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous
 
 # ======== MAIN
 def main():
 	buffer = ''
-	pkt_list, pktTime, pktLen, prevPkt, dataRate = [], [], [], [], []
-	i, prevTime, totalPktLen = 0, 0, 0
-	
-	# frequencies of data generation. Each number corresponds to in how many CPU cycles the parameter is generated
-	frequency_buffer = 3 
-	frequency_camera_control = 7 
-	frequency_land = 3
-	frequency_pitch = 2
-	frequency_takeoff = 5 
-	frequency_throttle = 1 
+	pkt_list, pkt_interarrival, pkt_length, pkt_previous, datarate = [], [], [], [], []
+	i, time_previous, pkt_length_total = 0, 0, 0
+	downlink, uplink = False, False
 
-	# control commands
-	camera_control = 'c' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
-	land = 'l' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
-	takeoff = 'a' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6]) 
-	throttle =  't' * 1 + 'r' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
-	pitch = 'p' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+	# ask user for which channel to generate data
+	while True: 
+		user_input = input("Select channel. Enter [d] for downlink (UAV -> RC) and [u] for uplink (RC -> UAV).\n")
+		if user_input in ['d', 'D']:
+			downlink = True
+			filename_extension = '_downlink'
+			break
+		elif user_input in ['u', 'U']:
+			uplink = True
+			filename_extension = '_uplink'
+			break
+		else:
+			print("Your input is NOT valid.")
 
 	print("\nPress [Enter] to start/end traffic generation.\n")
 	while True: 
@@ -72,62 +104,98 @@ def main():
 	# main loop
 	while True:
 		# APPLICATION LAYER. Generate data based on the application
-		if i % frequency_throttle == 0:
-			buffer += (throttle)
-		if i % frequency_pitch == 0:
-			buffer += (pitch)
-		if i % frequency_land == 0:
-			buffer += (land)
-		if i % frequency_takeoff == 0:
-			buffer += (takeoff)
-		if i % frequency_camera_control == 0:
-			buffer += (camera_control)
+		if downlink:
+			# control data
+			camera_control = 'c' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			land = 't' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			pitch = 'r' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			return_home = 'h' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			record_video = 'c' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			roll = 'r' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			takeoff = 't' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6]) 
+			throttle =  'l' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			trim = 'h' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			yaw =  'l' * np.random.choice([2**1, 2**2, 2**3, 2**4, 2**5, 2**6])
+			
+			# add data to UDP buffer
+			if i % frequency_throttle_yaw == 0:
+				buffer += (throttle)
+				buffer += (yaw)
+			if i % frequency_pitch_roll == 0:
+				buffer += (pitch)
+				buffer += (roll)
+			if i % frequency_land_takeoff == 0:
+				buffer += (land)
+				buffer += (takeoff)
+			if i % frequency_return_home_trim == 0:
+				buffer += (return_home)
+				buffer += (trim)
+			if i % frequency_camera_control_record_video == 0:
+				buffer += (camera_control)
+				buffer += (record_video)
+		else:
+			# telemetry data
+			rotorstatus = 'o' * np.random.choice([2**5, 2**6, 2**7])
+			camerastatus = 'm' * np.random.choice([2**5, 2**6, 2**7])
+			batterystatus =  'b' * np.random.choice([2**5, 2**6, 2**7])
+			imustatus = 'i' * np.random.choice([2**5, 2**6, 2**7])
+
+			# video data
+			video = 'v' * int(np.random.uniform(5000, 8000)) # range is based on the actual measured video data from DJI Spark
+
+			# add data to UDP buffer
+			if i % frequency_video == 0:
+				buffer += (video)
+			if i % frequency_imustatus_rotorstatus == 0:
+				buffer += (rotorstatus)
+				buffer += (imustatus)
+			if i % frequency_batterystatus_camerastatus == 0:
+				buffer += (camerastatus)
+				buffer += (batterystatus)
 		
 		# UDP LAYER. Check the UDP buffer and generate packets
 		if i % frequency_buffer == 0: 
-				k = 0
+				j, k = 0, 0
+
 				while True:
-					if k == len(buffer) or len(buffer) == 0:
+					if (downlink and (k == len(buffer) or len(buffer) == 0)) or (uplink and (j == math.ceil(len(buffer) / pkt_length_maximum) - 1)):
 						break
 					delayProb = np.random.uniform(0, 1)
 					
 					#if buffer[len(buffer) - 1 - k] == 't' and buffer[len(buffer) - 2 - k] != 't':
-					if buffer[len(buffer) - 1 - k] != buffer[len(buffer) - 2 - k]:
+					if (downlink and (buffer[len(buffer) - 1 - k] != buffer[len(buffer) - 2 - k])) or uplink:
 						if delayProb > 0.8:
-							sleepTime = np.random.uniform(0, buffercheck_frequency / 10)
-							time.sleep(sleepTime)
+							time_sleep = np.random.uniform(0, frequency_buffer / 10)
+							time.sleep(time_sleep)
 
+					if downlink and (buffer[len(buffer) - 1 - k] != buffer[len(buffer) - 2 - k]):
 						pkt = pkt_create(buffer[len(buffer) - 1 - k:]) # add UDP and IP headers
 						buffer = buffer[:len(buffer) - 1 - k]
-						timeDiff = float(pkt.time - prevTime) * 10 if prevTime != 0 else 0 # multiply by 10 to convert into ms
-						pktTime.append(timeDiff)
-						pktLen.append(len(pkt))
-						if int(prevTime / 100) != int(pkt.time / 100): # divide by 100 to convert into sec
-							if not firstrun: dataRate.append(totalPktLen * 8 / 1000) # multiply by 8 to convert bytes to bits, divide by 1000 to convert into kbps
-							totalPktLen = 0
-							firstrun = False
-						else:
-							dataRate.append("")
-						totalPktLen += len(pkt)	
-						print(pkt.show())
-						pkt_list.append(pkt) # list_pkt are the generated packets to be sent to the MAC layer for transmission
-						prevTime = pkt.time
+						datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous = generate_stats(datarate, 
+								firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous)
 						k = 0
-					else: 
+					elif downlink: 
 						k += 1
-				print("Num of gen. pkts: %d" %len(pktTime))
+					elif uplink:
+						pkt = pkt_create(buffer[len(buffer) - 1 - pkt_length_maximum:]) # add UDP and IP headers
+						buffer = buffer[:len(buffer) - 1 - pkt_length_maximum]
+						datarate, firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous = generate_stats(datarate, 
+								firstrun, pkt, pkt_interarrival, pkt_length, pkt_length_total, pkt_list, time_previous)
+						j += 1
+				print("Num of gen. pkts: %d" %len(pkt_interarrival))
 		i += 1
-		
-		with open(outputfolder + os.sep + date + '.csv', 'w') as outputFile:
-			outputFile.write("{}, {}, {}\n".format("Packet Inter-arrival (ms)", "Packet Length (bytes)", "Data Rate (kbps)"))
-			for x in zip(pktTime, pktLen, dataRate):
-				outputFile.write("{}, {}, {}\n".format(x[0], x[1], x[2]))
 		
 		if keyboard.is_pressed('\n'):
 			break
 		time.sleep(0.1)
 	
-	wrpcap(outputfolder + os.sep + date + '.pcap', pkt_list)
+	# save pkts to a pcap file
+	wrpcap(outputfolder + os.sep + date + filename_extension + '.pcap', pkt_list)
+	# save stats to a csv file
+	with open(outputfolder + os.sep + date + filename_extension + '.csv', 'w') as outputfile: 
+		outputfile.write("{}, {}, {}\n".format("Packet Inter-arrival (ms)", "Packet Length (bytes)", "Data Rate (kbps)"))
+		for x in zip(pkt_interarrival, pkt_length, datarate):
+			outputfile.write("{}, {}, {}\n".format(x[0], x[1], x[2]))
 	print("Packet generation is completed!")
 
 main()
--- uplink.py
+++ uplink.py
@@ -6,7 +6,7 @@
 # This code generates UAV data traffic for
 # uplink channel (from remote controller to UAV).
 # 
-# Prerequisites: pip3 install msvcrt numpy scapy
+# Prerequisites: pip3 install keyboard numpy scapy
 # 
 # Author: Aygün Baltaci
 # Institution: Technical University of Munich
@@ -24,22 +24,26 @@ import keyboard
 import time
 import numpy as np
 
-# ======== variables
+# ======== variables - modify them as you wish
 outputfolder = "outputfiles"
 ip_source = "10.0.0.201" 
 ip_destination = "10.0.0.208" 
 port_source = 47813 
+port_destination = 47814
 date = datetime.now().strftime('%Y%m%d_%H%M%S')
-buffercheck_frequency = 3 # in how many CPU cycles the buffer to be checked
+# frequencies of data generation. Each number corresponds to in how many CPU cycles the parameter is generated
+frequency_batterystatus_camerastatus = 6
+frequency_buffer = 3
+frequency_imustatus_rotorstatus = 2
+frequency_video = 1 
 
-def pkt_addLayers(pkt):
-	pkt = UDP()/pkt
-	pkt = IP()/pkt # 13082019
-	pkt.time = time.time() * 100
-	pkt[IP].src = srcIP
-	pkt[IP].dst = dstIP
-	pkt[UDP].sport = srcPort # Add a src port #
-	
+def pkt_create(payload):
+	pkt = IP() / UDP() / Raw(load = payload)
+	pkt = pkt
+	pkt[IP].src = ip_source
+	pkt[IP].dst = ip_destination
+	pkt[UDP].sport = port_source 
+	pkt[UDP].sport = port_destination
 	return pkt
 
 # ======== MAIN
@@ -48,28 +52,27 @@ def main():
 	pktList, pktTime, pktLen, prevPkt, dataRate = [], [], [], [], []
 	i, prevTime, totalPktLen = 0, 0, 0
 	
-	
-	maxPktLen = 1486
+	pkt_length_maximum = 1486
 
 	while True:
 		rotorstatus = 'r' * int(np.random.uniform(25, 125))
 		camerastatus = 'v' * int(np.random.uniform(25, 125)) 
 		batterystatus =  'b' * int(np.random.uniform(25, 125))
 		imustatus = 'i' * int(np.random.uniform(25, 125))
-		videoInformation = 'v' * int(np.random.uniform(5000, 8000))
-		if i % 1 == 0:
-			buffer += (videoInformation)
-		if i % 2 == 0:
+		video = 'v' * int(np.random.uniform(5000, 8000)) # range is based on the actual measured video data from DJI Spark
+		if i % frequency_video == 0:
+			buffer += (video)
+		if i % frequency_imustatus_rotorstatus == 0:
 			buffer += (rotorstatus)
 			buffer += (imustatus)
-		if i % 6 == 0:
+		if i % frequency_batterystatus_camerastatus == 0:
 			buffer += (camerastatus)
 			buffer += (batterystatus)
 		
 		if i % buffercheck_frequency == 0: # (not throttle is None or not pitch is None or cameraControl) and 
 			k = 0
 				
-			for j in range(math.ceil(len(buffer) / maxPktLen)):
+			for j in range(math.ceil(len(buffer) / pkt_length_maximum)):
 				delayProb = np.random.uniform(0, 1)
 				if delayProb > 0.8:
 					sleepTime = np.random.uniform(0, buffercheck_frequency / 10)
@@ -77,8 +80,8 @@ def main():
 
 				time.sleep(0.1) # TODO need this? 
 					
-				pkt = buffer[len(buffer) - 1 - maxPktLen:]
-				buffer = buffer[:len(buffer) - 1 - maxPktLen]
+				pkt = buffer[len(buffer) - 1 - pkt_length_maximum:]
+				buffer = buffer[:len(buffer) - 1 - pkt_length_maximum]
 				pkt = pkt_addLayers(pkt)
 				timeDiff = float(pkt.time - prevTime) if prevTime != 0 else 0
 				pktTime.append(timeDiff)
